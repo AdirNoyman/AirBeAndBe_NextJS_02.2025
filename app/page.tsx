bf0ca6069ed5f76b3bfb85046534545fdfd7bd0a
Adir Noyman <ofahol2007@hotmail.com>
2025-02-17 22:37:29 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const HomePage = () => {
+  return <>
+  <h1 className="text-3xl">HomePage 🤓</h1>
+  <Button variant='destructive' size='lg' className='capitalize'>Click me</Button>
+  </>;
+};
+
+export default HomePage;
